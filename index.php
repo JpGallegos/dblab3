ac8fb62ac58dd070bd8c2c002e45b7f427872f30
Jp Gallegos <jp.gallegos.diaz@gmail.com>
2015-04-19 12:52:06 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+    session_start()
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>Programa de Clases</title>
+    <link rel="stylesheet" src="http://maxcdn.bootstracdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></link>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Brand</a>
+            </div>
+            <div class="collapse navbar-collapse" id="#bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <link rel="script" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></link>
+    <link rel="script" src="http://maxcdn.bootstracdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></link>
+</body>
+</html>
